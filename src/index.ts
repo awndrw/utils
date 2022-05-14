0853ee9303eab911707a409e7e7c48910aabc767
Andrew Wiggin <andrew@wiggin.dev>
2022-05-14 01:59:06 -0400

    Export EventName and EventListener

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { events } from './events';
+export type { EventName, EventListener } from './events';
 export type { LiteralUnion, Callback } from './types';
 export { throttle } from './throttle';
 export { noop } from './noop';
